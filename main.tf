a905fa6ae458a14cf8a5b5fb83dbab7b22df9dd4
Eke Donald <eke.ikenna71@gmail.com>
2024-01-16 14:50:30 +0100

    updates

--- main.tf
+++ main.tf
@@ -345,29 +345,29 @@ module "vpc_cni_irsa" {
   tags = local.tags
 }
 
-#module "ebs_kms_key" {
-#  source  = "terraform-aws-modules/kms/aws"
-#  version = "~> 1.5"
-#
-#  description = "Customer managed key to encrypt EKS managed node group volumes"
+module "ebs_kms_key" {
+  source  = "terraform-aws-modules/kms/aws"
+  version = "~> 1.5"
 
-#  # Policy
-#  key_administrators = [
-#    data.aws_caller_identity.current.arn
-#  ]
+  description = "Customer managed key to encrypt EKS managed node group volumes"
 
-#  key_service_roles_for_autoscaling = [
-#    # required for the ASG to manage encrypted volumes for nodes
-#    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
-#    # required for the cluster / persistentvolume-controller to create encrypted PVCs
-#    module.eks.cluster_iam_role_arn,
-#  ]
+  # Policy
+  key_administrators = [
+    data.aws_caller_identity.current.arn
+  ]
 
-#  # Aliases
-#  aliases = ["eks/${local.name}/ebs"]
-#
-#  tags = local.tags
-#}
+  key_service_roles_for_autoscaling = [
+    # required for the ASG to manage encrypted volumes for nodes
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
+    # required for the cluster / persistentvolume-controller to create encrypted PVCs
+    module.eks.cluster_iam_role_arn,
+  ]
+
+  # Aliases
+  aliases = ["eks/${local.name}/ebs"]
+
+  tags = local.tags
+}
 
 module "key_pair" {
   source  = "terraform-aws-modules/key-pair/aws"
