0a2b139051462a4a7efbda6ea48f82f9f680182c
Eke Donald <eke.ikenna71@gmail.com>
2024-01-17 13:21:46 +0100

    updates

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "ekeikenna-buckets1"
+    bucket = "ekeikenna-buckets12"
     key    = "terraform.tfstate"
     region = "eu-west-2"
   }
